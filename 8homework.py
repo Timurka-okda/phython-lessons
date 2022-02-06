c11f1f68820a1768110b2246d29dbf023a532bf3
Timurka-okda <75175364+Timurka-okda@users.noreply.github.com>
2022-02-06 16:31:16 +0200

    Create 8homework.py

---
+++ 8homework.py
@@ -0,0 +1,41 @@
+# Task_1
+# names = ["Vanya", "Petya", "Vasya", "Timur", "Gleb", "Tom", "Michael"]
+# for i in names:
+#     print(i)
+# 
+# 
+# Task_2
+# for i in names:
+#     print(f"Hello, {i}!")
+
+# Task_3
+# motorcycles = ["Honda", "Suzuki", "Yamaha", "Kawasaki", "BMW", "KTM"]
+# for i in motorcycles:
+#     print(f"Я хотел бы купить мотоцикл {i}")
+# import time
+#
+#
+# def invite(massive):
+#     for i in massive:
+#         print(f"{i}, приглашаю тебя на обед в ресторане 'All Inclusive'!!! Встреча в 15:30!!! ")
+#         time.sleep(0.3)
+#
+#
+# # Task_4
+# people = ["Лиза", "Артем", "Настя", "Даня"]
+# invite(people)
+#
+# # Task_5
+# time.sleep(2)
+# print("От Насти: Я не смогу прийти, у меня куча дел, прости(")
+# time.sleep(2)
+# people.remove("Настя")  # or people.pop(2)
+# people.insert(2, "Арина")
+# invite(people)
+#
+# # Task_6
+# print("Я решил купить стол побольше чтобы вместилось еще 3 человека. Я приглашу еще Арину, Дашу и Дарину!")
+# people.insert(0, "Арина")
+# people.insert(3, "Даша")
+# people.append("Дарина")
+# invite(people)
